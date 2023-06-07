406d8708101bd755918d1aaf2b1bce4f487e02bf
Sam Price <samuel.r.price@nasa.gov>
2023-06-07 09:59:52 -0400

    Add example of writing out file.

--- example/src_py/test_extended.py
+++ example/src_py/test_extended.py
@@ -14,6 +14,21 @@ except:
   
 print(health.dac)
 
+# NEXT STEPS
+# iterate through all fields
+# Make a deep copy of the parent struct
+# for each range / enum attribute
+# Set the field
+# Save the data to a file
+
+# After that we need to disable the validation, 
+# and set the ranges outside of the valid range.
+
+# More context
+# Health will be initalized with valid data initally
+# We will modify it to become invalid on a per parameter basis 
+
+file_counter = 0
 for f in health._efields_:
   
   fname = f[0]
@@ -21,5 +36,7 @@ for f in health._efields_:
   print(fname, fpart.range)
   if fpart.range:
     # TODO somehow disable validation  check...
-    setattr(health, fname, fpart.range[0] - 1)
-    setattr(health, fname, fpart.range[1] + 1)
+    setattr(health, fname, fpart.range[0] )
+    with open(f'{type(health).__name__}_{file_counter}.bin', 'wb') as fid:
+      fid.write(health)
+      file_counter += 1
