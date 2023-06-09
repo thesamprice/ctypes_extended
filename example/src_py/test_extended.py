8490cb49b69e75242ad5ca5f71b12ee0dfc34a0a
Trehan <ntrehan@ndc.nasa.gov>
2023-06-09 14:34:29 -0400

    test files added

--- example/src_py/test_extended.py
+++ example/src_py/test_extended.py
@@ -2,7 +2,8 @@
 import ectypes_gps as gps
 #import pytest
 import copy as cpy
- 
+import importlib
+import inspect
 
 health = gps.Health_M()
 health.dac = 11.9
@@ -30,3 +31,11 @@ for f in health._efields_:
     with open(f'{type(health).__name__}_{file_counter}.bin', 'wb') as fid:
       fid.write(health)
       file_counter += 1
+
+
+#scan Python modules for command/telemetry data
+module1 = importlib.import_module(tlm_sqllite)
+
+for name, obj in inspect.getmembers(module1):
+  if name.find('ctype') != -1:
+    print(f'found ctype struct: {name}')
